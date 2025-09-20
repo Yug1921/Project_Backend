252c5c26ddec1f301accf8eea59672c8c921c494
Yug1921 <yugyugupadhyay@gmail.com>
2025-09-20 22:41:28 +0530

    getUserChannelProfile - user.controller.js, fetched channel and subscribers data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -291,7 +291,7 @@ const getCurrentUser = asyncHandler(async (req, res) => {
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
 
-  if (!fullName || email) {
+  if (!fullName || !email) {
     throw new ApiError(400, 'All fields are required');
   }
 
@@ -324,7 +324,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     throw new ApiError(400, 'Avatar upload failed');
   }
 
-  const user = await findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -352,7 +352,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     throw new ApiError(400, 'Cover Image upload failed');
   }
 
-  const user = await findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -365,6 +365,77 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .json(new ApiResponse(200, user, 'Cover Image updated successfully'));
+});
+
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim) {
+    throw new ApiError(400, 'username not found');
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        username: username?.toLowerCase,
+      },
+    },
+    {
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'channel',
+        as: 'subscribers',
+      },
+    },
+    {
+      $lookup: {
+        from: 'subscriptions',
+        localField: '_id',
+        foreignField: 'subscriber',
+        as: 'subscribedTo',
+      },
+    },
+    {
+      $addFields: {
+        subcribersCount: {
+          $size: '$subscribers',
+        },
+        channelsSubcribedTo: {
+          $size: '$subscribedTo',
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, '$subscribers.subscriber'] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        subcribersCount: 1,
+        channelsSubcribedTo: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(400, 'No channel found');
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, channel[0], 'User channel fetched successfully')
+    );
 });
 
 export {
